35005883096f32eb583e112223be4d661dc6754f
Meghan Gutshall <meghan.gutshall@gmail.com>
2018-07-26 00:21:21 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,17 +1,18 @@
 def starts_with_a_vowel?(word)
-  if word.match(/\b[aeiou]/i)
+  if word.match(/^[aeiou]/i)
     true
-  elsif word.match(/\b[b-df-hj-np-tv-z]/i)
+  elsif word.match(/^[b-df-hj-np-tv-z]/i)
     false
   end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  text.scan(/un\w+ing/)
+  text.scan(/\bun\w+ing\b/)
 end
 
 def words_five_letters_long(text)
   text.scan(/\b[a-z]{5}\b/i)
+  # or text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
@@ -24,7 +25,7 @@ end
 
 def valid_phone_number?(phone)
   # has to have ten digits and ignore spaces, hyphens, and parentheses
-  if phone.match(/.?\d{3}.?\d{3}.?\d{4}$/)
+  if phone.match(/.*?\d{3}.*?\d{3}.*?\d{4}$/)
     true
   else
     false
